ac48da991065244fdd0b79d49879416eb5c71397
Brandon Minnick <Brandon.r.minnick@gmail.com>
2016-09-23 07:34:30 -0700

    Upate Emotion NuGet

--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -26,6 +26,7 @@ namespace FaceOff.Droid
 		
 		public static void UpdateIdValues()
 		{
+			global::Xamarin.Forms.Platform.Android.Resource.Attribute.actionBarSize = global::FaceOff.Droid.Resource.Attribute.actionBarSize;
 		}
 		
 		public partial class Animation
