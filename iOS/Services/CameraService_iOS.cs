7d074fcc6e1727cf3c6a5b04318fbcf364f2ad3d
brminnick <brandon.r.minnick@gmail.com>
2016-11-02 20:15:26 -0700

    Added Camera Overlay for iOS

---
+++ iOS/Services/CameraService_iOS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using UIKit;
+
+namespace FaceOff.iOS
+{
+	public class CameraService_iOS : ICameraService
+	{
+		public Func<object> GetCameraOverlay()
+		{
+			return () =>
+		  	{
+				var imageView = new UIImageView(UIImage.FromBundle("face-template-overlay.png"));
+				imageView.ContentMode = UIViewContentMode.ScaleAspectFit;
+
+				var screen = UIScreen.MainScreen.Bounds;
+				imageView.Frame = screen;
+
+				return imageView;
+			};
+		}
+	}
+}
