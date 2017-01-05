d16c24ffc090005a397b55221eccdef0c06868cd
brminnick <brandon.r.minnick@gmail.com>
2017-01-05 15:13:09 -0800

    Added Entry Functionality

---
+++ Droid/Custom Renderers/EntryWithCustomKeyboardReturnButtonCustomRenderer.cs
@@ -0,0 +1,65 @@
+﻿using Android.Widget;
+using Android.Views.InputMethods;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using FaceOff;
+using FaceOff.Droid;
+
+[assembly: ExportRenderer(typeof(EntryWithCustomKeyboardReturnButton), typeof(EntryWithCustomKeyboardReturnButtonCustomRenderer))]
+namespace FaceOff.Droid
+{
+	public class EntryWithCustomKeyboardReturnButtonCustomRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			var customEntry = Element as EntryWithCustomKeyboardReturnButton;
+
+			if (Control != null && customEntry != null)
+			{
+				SetReturnType(customEntry);
+
+				Control.EditorAction += (object sender, TextView.EditorActionEventArgs args) =>
+				{
+					if (customEntry?.ReturnType != ReturnType.Next)
+						customEntry?.Unfocus();
+
+					customEntry?.InvokeCompleted();
+				};
+			}
+		}
+
+		void SetReturnType(EntryWithCustomKeyboardReturnButton entry)
+		{
+			var type = entry.ReturnType;
+
+			switch (type)
+			{
+				case ReturnType.Go:
+					Control.ImeOptions = ImeAction.Go;
+					Control.SetImeActionLabel("Go", ImeAction.Go);
+					break;
+				case ReturnType.Next:
+					Control.ImeOptions = ImeAction.Next;
+					Control.SetImeActionLabel("Next", ImeAction.Next);
+					break;
+				case ReturnType.Send:
+					Control.ImeOptions = ImeAction.Send;
+					Control.SetImeActionLabel("Send", ImeAction.Send);
+					break;
+				case ReturnType.Search:
+					Control.ImeOptions = ImeAction.Search;
+					Control.SetImeActionLabel("Search", ImeAction.Search);
+					break;
+				default:
+					Control.ImeOptions = ImeAction.Done;
+					Control.SetImeActionLabel("Done", ImeAction.Done);
+					break;
+			}
+		}
+	}
+}
+
---
+++ iOS/Custom Renderers/EntryWithCustomKeyboardReturnButtonCustomRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using FaceOff;
+using FaceOff.iOS;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(EntryWithCustomKeyboardReturnButton), typeof(EntryWithCustomKeyboardReturnButtonCustomRenderer))]
+namespace FaceOff.iOS
+{
+	public class EntryWithCustomKeyboardReturnButtonCustomRenderer : EntryRenderer
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+		{
+			base.OnElementChanged(e);
+
+			var customEntry = Element as EntryWithCustomKeyboardReturnButton;
+
+			if (Control != null && customEntry != null)
+			{
+				SetReturnType(customEntry);
+
+				Control.ShouldReturn += (UITextField tf) =>
+				{
+					customEntry?.InvokeCompleted();
+					return true;
+				};
+			}
+		}
+
+		void SetReturnType(EntryWithCustomKeyboardReturnButton entry)
+		{
+			var type = entry.ReturnType;
+
+			switch (type)
+			{
+				case ReturnType.Go:
+					Control.ReturnKeyType = UIReturnKeyType.Go;
+					break;
+				case ReturnType.Next:
+					Control.ReturnKeyType = UIReturnKeyType.Next;
+					break;
+				case ReturnType.Send:
+					Control.ReturnKeyType = UIReturnKeyType.Send;
+					break;
+				case ReturnType.Search:
+					Control.ReturnKeyType = UIReturnKeyType.Search;
+					break;
+				case ReturnType.Done:
+					Control.ReturnKeyType = UIReturnKeyType.Done;
+					break;
+				default:
+					Control.ReturnKeyType = UIReturnKeyType.Default;
+					break;
+			}
+		}
+	}
+}
