b51784e9048cb33e422a1047cb44a71c68162660
brminnick <brandon.r.minnick@gmail.com>
2017-05-19 08:38:12 -0700

    Updated NuGet Packages add C#7 Features

--- FaceOff/ViewModels/WelcomeViewModel.cs
+++ FaceOff/ViewModels/WelcomeViewModel.cs
@@ -5,14 +5,14 @@
 		#region Properties
 		public string Player1
 		{
-			get { return Settings.Player1Name; }
-			set { Settings.Player1Name = value; }
+			get => Settings.Player1Name;
+			set => Settings.Player1Name = value;
 		}
 
 		public string Player2
 		{
-			get { return Settings.Player2Name; }
-			set { Settings.Player2Name = value; }
+			get => Settings.Player2Name;
+			set => Settings.Player2Name = value;
 		}
 		#endregion
 	}
