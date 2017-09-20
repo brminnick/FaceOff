2dd029f48daaf718d12220fbd333e21ea02030eb
brminnick <brandon.r.minnick@gmail.com>
2017-09-19 23:04:18 -0500

    Updated NuGet Packages

--- UITests/Tests/ReplTests.cs
+++ UITests/Tests/ReplTests.cs
@@ -6,17 +6,17 @@ using Xamarin.UITest;
 
 namespace FaceOff.UITests
 {
-	public class ReplTests : BaseTest
-	{
-		public ReplTests(Platform platform):base(platform)
-		{
-		}
+    public class ReplTests : BaseTest
+    {
+        public ReplTests(Platform platform) : base(platform)
+        {
+        }
 
-		[Test]
-		public void Repl()
-		{
-			app.Repl();
-		}
-	}
+        [Test, Ignore]
+        public void Repl()
+        {
+            app.Repl();
+        }
+    }
 }
 
