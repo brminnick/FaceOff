fcddc1bc50bdf6543aab7325bae8a559a442035f
brminnick <brandon.r.minnick@gmail.com>
2017-11-05 22:16:51 -0500

    Updated EmotionApiKey

--- FaceOff/Constants/CognitiveServicesConstants.cs
+++ FaceOff/Constants/CognitiveServicesConstants.cs
@@ -2,8 +2,8 @@
 {
 	public static class CognitiveServicesConstants
     {
-//#error Emotion API Key Missing! Sign up for a free API Key
+#error Emotion API Key Missing! Sign up for a free API Key
         //Sign up for a free API Key: https://www.microsoft.com/cognitive-services/en-us/emotion-api
-        public const string EmotionApiKey = "64244430cff44a14ae12359ce4ecbf66";
+        public const string EmotionApiKey = "Add Emotion API Key Here";
     }
 }
