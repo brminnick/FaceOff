1352ab9c28b5480befa132814e8b790877ed43b1
brminnick <brandon.r.minnick@gmail.com>
2018-02-04 14:48:43 -0800

    Removed Unused Folder

--- FaceOff/Constants/CognitiveServicesConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FaceOff
-{
-	public static class CognitiveServicesConstants
-    {
-#error Emotion API Key Missing! Sign up for a free API Key    
-        //Sign up for a free API Key: https://aka.ms/myvcp0
-        public const string EmotionApiKey = "Add Emotion API Key Here";
-    }
-}
