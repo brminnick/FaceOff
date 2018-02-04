8036fa4436519039f6ba0abf140fb3ebf069a44f
brminnick <brandon.r.minnick@gmail.com>
2018-02-04 13:58:09 -0800

    Updated Folder Names

---
+++ Source/FaceOff/Constants/CognitiveServicesConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace FaceOff
+{
+	public static class CognitiveServicesConstants
+    {
+//#error Emotion API Key Missing! Sign up for a free API Key    
+        //Sign up for a free API Key: https://www.microsoft.com/cognitive-services/en-us/emotion-api
+        public const string EmotionApiKey = "c5e6fb0ba372432f82f53ba462fdedc4";
+    }
+}
