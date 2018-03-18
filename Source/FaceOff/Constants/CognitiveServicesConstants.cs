07c00d6e57823e358c4364fa6fe7abba4819ccac
Brandon Minnick <brminnick@users.noreply.github.com>
2018-03-18 12:23:13 -0400

    Update CognitiveServicesConstants.cs

--- Source/FaceOff/Constants/CognitiveServicesConstants.cs
+++ Source/FaceOff/Constants/CognitiveServicesConstants.cs
@@ -3,7 +3,7 @@
 	public static class CognitiveServicesConstants
     {
 #error Emotion API Key Missing! Sign up for a free API Key    
-        //Sign up for a free API Key: https://aka.ms/myvcp0
+        //Sign up for a free API Key: https://aka.ms/CognitiveServices/EmotionAPI
         public const string EmotionApiKey = "Add Emotion API Key Here";
     }
 }
