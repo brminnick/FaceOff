68f45ad91deb9d5eaaba92888aae5b1fbf703585
Brandon Minnick <brminnick@users.noreply.github.com>
2018-04-13 12:31:05 -0700

    Added Post Build Scripts

--- Source/appcenter-pre-build.sh
+++ Source/appcenter-pre-build.sh
@@ -3,4 +3,9 @@ APIScriptPath=`find . -name injectCognitiveServicesAPI.sh | head -1`
 
 CognitiveServicesConstantsFile=`find . -name CognitiveServicesConstants.cs | head -1`
 
+echo APIScriptPath = $APIScriptPath
+echo CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
+
 bash $APIScriptPath $CognitiveServicesConstantsFile $CognitiveServicesAPIKey
+
+echo "Finished Injecting Cognitive Services API Key"
