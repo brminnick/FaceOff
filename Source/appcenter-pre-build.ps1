3c884e05b82ce44c08acebf073938d20ba978a3e
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-02 16:33:14 -0700

    Added Quotes to path

--- Source/appcenter-pre-build.ps1
+++ Source/appcenter-pre-build.ps1
@@ -1,6 +1,6 @@
-APIScriptPath = Get-ChildItem -Path $APPCENTER_SOURCE_DIRECTORY\* -Filter "CognitiveServicesConstants.cs" -Recurse -File
+APIScriptPath = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY"\* -Filter "CognitiveServicesConstants.cs" -Recurse -File
 
-CognitiveServicesConstantsFile = Get-ChildItem -Path $APPCENTER_SOURCE_DIRECTORY\* -Filter "injectCognitiveServicesAPI.sh" -Recurse -File
+CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY"\* -Filter "injectCognitiveServicesAPI.sh" -Recurse -File
 
 Write-Host APIScriptPath = $APIScriptPath
 Write-Host CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
