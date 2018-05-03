9667fb6ab3058ba08331e9575fff032eea4f3632
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-02 17:15:13 -0700

    Fixed bug

--- Source/appcenter-pre-build.ps1
+++ Source/appcenter-pre-build.ps1
@@ -1,6 +1,6 @@
-$APIScriptPath = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY\*" -Filter "CognitiveServicesConstants.cs" -Recurse -File | % { $_.FullName }
+$APIScriptPath = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY\*" -Filter "injectCognitiveServicesAPI.sh" -Recurse -File | % { $_.FullName }
 
-$CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY\*" -Filter "injectCognitiveServicesAPI.sh" -Recurse -File | % { $_.FullName }
+$CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY\*" -Filter  "CognitiveServicesConstants.cs" -Recurse -File | % { $_.FullName }
 
 Write-Host APIScriptPath = $APIScriptPath
 Write-Host CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
