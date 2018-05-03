a6c99942728b544ac9f53def211ddb78351880b9
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-02 17:47:15 -0700

    Added Get-Content Set-Content

--- Source/appcenter-pre-build.ps1
+++ Source/appcenter-pre-build.ps1
@@ -2,7 +2,7 @@ $CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTO
 
 Write-Host CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
 
-cat $CognitiveServicesConstantsFile | % { $_ -replace "Add Face API Key Here", $CognitiveServicesAPIKey }
-cat $CognitiveServicesConstantsFile | % { $_ -replace "#error", "//#error" }
+(Get-Content $CognitiveServicesConstantsFile).replace("Add Face API Key Here", $CognitiveServicesAPIKey) | Set-Content $CognitiveServicesConstantsFile
+(Get-Content $CognitiveServicesConstantsFile).replace("#error", "//#error") | Set-Content $CognitiveServicesConstantsFile
 
 Write-Host "Finished Injecting Cognitive Services API Key"
