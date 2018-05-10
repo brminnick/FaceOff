57707ddaeb6accfbafb1fbf237f3fc38da1bc9e8
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-10 12:14:13 -0700

    Updated for CrossCurrentActivity 2.x

--- Source/FaceOff.Droid/MainApplication.cs
+++ Source/FaceOff.Droid/MainApplication.cs
@@ -8,47 +8,48 @@ using Plugin.CurrentActivity;
 
 namespace FaceOff.Droid
 {
-	[Application]
-	public class MainApplication : Application, Application.IActivityLifecycleCallbacks
-	{
-		public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
-		{
-		}
+    [Application]
+    public class MainApplication : Application, Application.IActivityLifecycleCallbacks
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership transer) : base(handle, transer)
+        {
+        }
 
-		public override void OnCreate()
-		{
-			base.OnCreate();
-			RegisterActivityLifecycleCallbacks(this);
-		}
+        public override void OnCreate()
+        {
+            base.OnCreate();
+            CrossCurrentActivity.Current.Init(this);
+            RegisterActivityLifecycleCallbacks(this);
+        }
 
-		public override void OnTerminate()
-		{
-			base.OnTerminate();
-			UnregisterActivityLifecycleCallbacks(this);
-		}
+        public override void OnTerminate()
+        {
+            base.OnTerminate();
+            UnregisterActivityLifecycleCallbacks(this);
+        }
 
-		public void OnActivityCreated(Activity activity, Bundle savedInstanceState) => CrossCurrentActivity.Current.Activity = activity;
+        public void OnActivityCreated(Activity activity, Bundle savedInstanceState) => CrossCurrentActivity.Current.Activity = activity;
 
-		public void OnActivityResumed(Activity activity) => CrossCurrentActivity.Current.Activity = activity;
+        public void OnActivityResumed(Activity activity) => CrossCurrentActivity.Current.Activity = activity;
 
-		public void OnActivityStarted(Activity activity) => CrossCurrentActivity.Current.Activity = activity;
+        public void OnActivityStarted(Activity activity) => CrossCurrentActivity.Current.Activity = activity;
 
-		public void OnActivityDestroyed(Activity activity)
-		{
-		}
+        public void OnActivityDestroyed(Activity activity)
+        {
+        }
 
-		public void OnActivityPaused(Activity activity)
-		{
-		}
-
-
-		public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
-		{
-		}
+        public void OnActivityPaused(Activity activity)
+        {
+        }
 
 
-		public void OnActivityStopped(Activity activity)
-		{
-		}
-	}
+        public void OnActivitySaveInstanceState(Activity activity, Bundle outState)
+        {
+        }
+
+
+        public void OnActivityStopped(Activity activity)
+        {
+        }
+    }
 }
