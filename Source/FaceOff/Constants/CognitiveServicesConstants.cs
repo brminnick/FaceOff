2c459332b6c95b7c391dcbbcdfb7408b8ca84fc1
Brandon Minnick <brminnick@users.noreply.github.com>
2018-05-10 22:55:46 -0700

    Revert "Enabled Bundle Assemblies"
    This reverts commit e1783932c56bf5463deda1c120787bbb0bb6056f.

---
+++ Source/FaceOff/Constants/CognitiveServicesConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace FaceOff
+{
+	public static class CognitiveServicesConstants
+    {
+#error Face API Key Missing! Sign up for a free API Key    
+        //Sign up for a free API Key: https://aka.ms/CognitiveServices/FaceAPI
+        public const string FaceApiKey = "Add Face API Key Here";
+        public const string FaceApiUrl = "https://westus.api.cognitive.microsoft.com/face/v1.0";
+    }
+}
