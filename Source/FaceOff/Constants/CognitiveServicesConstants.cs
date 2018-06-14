d19c57cf9dacfb4763d3a52944c70028dff38fe1
Brandon Minnick <brminnick@users.noreply.github.com>
2018-06-14 08:47:20 +0200

    Removed Unused ApiUrl

--- Source/FaceOff/Constants/CognitiveServicesConstants.cs
+++ Source/FaceOff/Constants/CognitiveServicesConstants.cs
@@ -5,6 +5,5 @@
 #error Face API Key Missing! Sign up for a free API Key    
         //Sign up for a free API Key: https://aka.ms/CognitiveServices/FaceAPI
         public const string FaceApiKey = "Add Face API Key Here";
-        public const string FaceApiUrl = "https://westus.api.cognitive.microsoft.com/face/v1.0";
     }
 }
