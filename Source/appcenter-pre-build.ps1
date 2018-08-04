311136c4d3e3e9631b7c0aff87d64288997c96d7
brminnick <brminnick@users.noreply.github.com>
2018-08-03 18:44:51 -0700

    Added BaseUrl

--- Source/appcenter-pre-build.ps1
+++ Source/appcenter-pre-build.ps1
@@ -3,6 +3,9 @@ $CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTO
 Write-Host CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
 
 (Get-Content $CognitiveServicesConstantsFile).replace("Add Face API Key Here", $CognitiveServicesAPIKey) | Set-Content $CognitiveServicesConstantsFile
-(Get-Content $CognitiveServicesConstantsFile).replace("#error", "//#error") | Set-Content $CognitiveServicesConstantsFile
+(Get-Content $CognitiveServicesConstantsFile).replace("#error Face API Key Missing", "//#error Face API Key Missing") | Set-Content $CognitiveServicesConstantsFile
+
+(Get-Content $CognitiveServicesConstantsFile).replace("Add Face API Base Url Here", $CognitiveServicesBaseUri) | Set-Content $CognitiveServicesConstantsFile
+(Get-Content $CognitiveServicesConstantsFile).replace("#error Base Url Missing", "//#error Base Url Missing") | Set-Content $CognitiveServicesConstantsFile
 
 Write-Host "Finished Injecting Cognitive Services API Key"
