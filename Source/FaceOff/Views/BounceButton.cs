f51bc5a5db8a7dc0dc7871b3782ec13b5f5a7b76
Brandon Minnick <brminnick@users.noreply.github.com>
2018-09-14 14:42:41 +1000

    Removed Animation Flag

--- Source/FaceOff/Views/BounceButton.cs
+++ Source/FaceOff/Views/BounceButton.cs
@@ -6,10 +6,6 @@ namespace FaceOff
 {
     public class BounceButton : Button
     {
-        #region Fields
-        bool _isBounceButtonAnimationInProgress = false;
-        #endregion
-
         #region Constructors
         public BounceButton()
         {
@@ -27,18 +23,13 @@ namespace FaceOff
         #region Methods
         void HandleButtonClick(object sender, EventArgs e)
         {
-            if (_isBounceButtonAnimationInProgress)
-                return;
-
             var bounceButton = (BounceButton)sender;
-            _isBounceButtonAnimationInProgress = true;
 
             Device.BeginInvokeOnMainThread(async () =>
             {
                 Unfocus();
                 await bounceButton?.ScaleTo(AnimationConstants.BounceButtonMaxSize, AnimationConstants.BounceButonAninmationTime);
                 await bounceButton?.ScaleTo(AnimationConstants.BounceButtonNormalSize, AnimationConstants.BounceButonAninmationTime);
-                _isBounceButtonAnimationInProgress = false;
             });
         }
         #endregion
