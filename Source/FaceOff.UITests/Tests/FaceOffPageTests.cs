e72cd8808431eba0d93b84989da99e46b7b42ba5
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-01-19 23:04:03 +0100

    Added Contempt

--- Source/FaceOff.UITests/Tests/FaceOffPageTests.cs
+++ Source/FaceOff.UITests/Tests/FaceOffPageTests.cs
@@ -91,7 +91,7 @@ namespace FaceOff.UITests
         }
 
         [TestCase(EmotionType.Anger, 100)]
-        //[TestCase(EmotionType.Contempt, 83.5)]
+        [TestCase(EmotionType.Contempt, 83.5)]
         [TestCase(EmotionType.Happiness, 100)]
         [TestCase(EmotionType.Sadness, 100)]
         public void VerifyPhoto1Results(EmotionType emotion, double expectedScore)
@@ -116,10 +116,10 @@ namespace FaceOff.UITests
         }
 
         [TestCase(EmotionType.Anger, 100)]
-        //[TestCase(EmotionType.Contempt, 83.5)]
+        [TestCase(EmotionType.Contempt, 83.5)]
         [TestCase(EmotionType.Happiness, 100)]
         [TestCase(EmotionType.Sadness, 100)]
-        public void VerifyPhoto2Results(EmotionType emotion, int expectedScore)
+        public void VerifyPhoto2Results(EmotionType emotion, double expectedScore)
         {
             //Arrange
 
