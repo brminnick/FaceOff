1b4046e895d22fc264c0588c83eff6ad8157e174
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2019-03-12 10:32:13 -0700

    Added AwsConstants

---
+++ Source/FaceOff/Constants/AwsConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace FaceOff
+{
+    public static class AwsConstants
+    {
+#error AWS Access Key Missing
+        public const string AccessKey = "Your Access Key";
+
+#error AWS Access Secret Missing
+        public const string AccessSecret = "Your Access Secret";
+    }
+}
