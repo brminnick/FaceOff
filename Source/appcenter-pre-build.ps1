f25cafadd611faf759bd659c736c41d7b22c567c
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2020-08-16 11:47:12 -0700

    Update appcenter-pre-build.ps1

--- Source/appcenter-pre-build.ps1
+++ Source/appcenter-pre-build.ps1
@@ -1,4 +1,4 @@
-$CognitiveServicesConstantsFile = Get-ChildItem -Path "$APPCENTER_SOURCE_DIRECTORY\*" -Filter  "CognitiveServicesConstants.cs" -Recurse -File | % { $_.FullName }
+$CognitiveServicesConstantsFile = Get-ChildItem -Path $APPCENTER_SOURCE_DIRECTORY -Filter  "CognitiveServicesConstants.cs" -Recurse -File | % { $_.FullName }
 
 Write-Host CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
 
