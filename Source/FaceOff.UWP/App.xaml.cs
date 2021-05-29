2e07a553e2cdf45ee60341014438fbaf531d7f75
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2021-05-28 17:30:25 -0700

    Fix Nullability

--- Source/FaceOff.UWP/App.xaml.cs
+++ Source/FaceOff.UWP/App.xaml.cs
@@ -36,7 +36,7 @@ namespace FaceOffUWP
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-            Frame rootFrame = Window.Current.Content as Frame;
+            var rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
