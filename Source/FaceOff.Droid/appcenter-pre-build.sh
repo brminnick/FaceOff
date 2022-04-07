7b25ba9f2bf169e85f4ca90d659d38eaacac1ecc
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2022-04-07 15:32:57 -0700

    Remove UWP Project from Solution

--- Source/FaceOff.Droid/appcenter-pre-build.sh
+++ Source/FaceOff.Droid/appcenter-pre-build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 PostBuildScriptFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name appcenter-post-build.sh | grep Droid | head -1`
 CognitiveServicesConstantsFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name CognitiveServicesConstants.cs | head -1`
+UWPCsproj=`find "$APPCENTER_SOURCE_DIRECTORY" -name FaceOff.UWP.csproj | head -1`
 SlnFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name FaceOff.sln | head -1`
 
 echo PostBuildScriptFile = $PostBuildScriptFile
@@ -18,6 +19,6 @@ echo "Finished Injecting Cognitive Services API Key"
 sed -i '' "s/--token token/--token $AppCenterLoginToken/g" "$PostBuildScriptFile"
 
 echo "Removing UWP Project"
-sed -i '' "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"FaceOff.UWP\", \"FaceOff.UWP\FaceOff.UWP.csproj\", \"{7E3407D5-2430-4086-8852-4C20F5B6F78F}\"" "$SlnFile"
+dotnet sln $SlnFile remove $UWPCsproj
 
 echo "Finished Injecting App Center Login Token"
--- Source/FaceOff.iOS/appcenter-pre-build.sh
+++ Source/FaceOff.iOS/appcenter-pre-build.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 PostBuildScriptFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name appcenter-post-build.sh | grep iOS | head -1`
 CognitiveServicesConstantsFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name CognitiveServicesConstants.cs | head -1`
+UWPCsproj=`find "$APPCENTER_SOURCE_DIRECTORY" -name FaceOff.UWP.csproj | head -1`
 SlnFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name FaceOff.sln | head -1`
 
 echo PostBuildScriptFile = $PostBuildScriptFile
 echo CognitiveServicesConstantsFile = $CognitiveServicesConstantsFile
+echo UWPCsproj = $UWPCsproj
 echo SlnFile = $SlnFile
 
 sed -i '' "s/Add Face API Key Here/$CognitiveServicesAPIKey/g" "$CognitiveServicesConstantsFile"
@@ -18,6 +20,6 @@ echo "Finished Injecting Cognitive Services API Key"
 sed -i '' "s/--token token/--token $AppCenterLoginToken/g" "$PostBuildScriptFile"
 
 echo "Removing UWP Project"
-sed -i '' "Project(\"{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}\") = \"FaceOff.UWP\", \"FaceOff.UWP\FaceOff.UWP.csproj\", \"{7E3407D5-2430-4086-8852-4C20F5B6F78F}\"" "$SlnFile"
+dotnet sln $SlnFile remove $UWPCsproj
 
 echo "Finished Injecting App Center Login Token"
